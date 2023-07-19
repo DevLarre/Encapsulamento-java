cc62baf903c58d99070201a78804206352b5fdf7
Douglas Larré <120336094+DevLarre@users.noreply.github.com>
2023-07-18 23:12:57 -0300

    Add files via upload

---
+++ stackx-orienta-o-a-objeto-encapsulamento-DevLarre/src/EntryPoint.java
@@ -0,0 +1,17 @@
+
+//Importe sua classe aqui
+
+class EntryPoint {
+    public static void main(String[] args) {
+        //Instancie sua classe aqui        
+    Pessoa pessoa1 = new Pessoa();
+
+    pessoa1.primeiroNome("Douglas");
+    pessoa1.sobrenome("Larré");
+    pessoa1.idade(26);
+    pessoa1.sexo("M");
+
+
+    pessoa1.imprimirPessoa();
+}   
+}
